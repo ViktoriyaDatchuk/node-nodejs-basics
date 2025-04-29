127355472d403193b6b550bc5a5af4d7b0dbe3e5
Viktoriya Datchuk <grelada@tut.by>
2025-04-29 16:41:37 +0300

    feat: done hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const stream = createReadStream(
+    join(__dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  stream.on("data", (chunk) => hash.update(chunk));
+
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+
+  stream.on("error", (error) => console.log("Error", error.message));
 };
 
 await calculateHash();

2cc5cdd754fecdf662bf0852407bc8aae4686ae5
Viktoriya Datchuk <grelada@tut.by>
2025-04-30 19:16:17 +0300

    feat: done streams module

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { createWriteStream } from "fs";
+import { stdin, stdout } from "process";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(
+    join(join(__dirname, "files", "fileToWrite.txt"))
+  );
+
+  stdout.write("Write your message\n");
+  stdin.on("data", (data) => {
+    const dataString = data.toString();
+    stream.write(dataString);
+    process.exit();
+  });
+
+  stdin.on("error", (error) => {
+    console.error("Error:", error.message);
+  });
 };
 
 await write();

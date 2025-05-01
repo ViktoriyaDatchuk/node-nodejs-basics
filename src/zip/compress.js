173b463dbb1536585533ea80192147e9871b4174
Viktoriya Datchuk <grelada@tut.by>
2025-05-01 20:22:46 +0300

    feat: done zip module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "node:zlib";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(
+    join(__dirname, "files", "fileToCompress.txt")
+  );
+  const destination = createWriteStream(join(__dirname, "files", "archive.gz"));
+
+  source.pipe(gzip).pipe(destination);
+
+  destination.on("finish", () => {
+    console.log("File compressed successfully");
+  });
+
+  source.on("error", (error) => {
+    console.error("Error reading file:", error.message);
+  });
+
+  destination.on("error", (error) => {
+    console.error("Error writing file:", error.message);
+  });
 };
 
 await compress();
